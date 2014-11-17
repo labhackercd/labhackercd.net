c89a1eed93db232b950c93f88980443e6a65c265
Dirley Rodrigues <dirleyrls@gmail.com>
2014-11-17 12:14:09 -0200

    Add default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,4 +107,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['assemble', 'sass', 'copy'])
 };
