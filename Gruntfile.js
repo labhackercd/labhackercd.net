6aa38d2c734195191b2171fc9fdff729789cf555
cfviotti <caio@viotti.me>
2016-09-13 21:44:05 -0300

    Update activity card for novo e-Democracia

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          port: 8000,
+          port: 8003,
           useAvaiablePort: true,
           //livereload: true,
           hostname: '*',
